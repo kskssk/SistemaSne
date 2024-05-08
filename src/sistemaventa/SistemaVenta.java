862811c4a7aad206007828ed51539e1cfede452b
Jesus Alberto Levario Flores <jesuslevarioflo@gmail.com>
2024-05-08 10:41:58 -0600

    Add files via upload

---
+++ src/sistemaventa/SistemaVenta.java
@@ -0,0 +1,18 @@
+
+package sistemaventa;
+
+import Vista.Login;
+
+
+public class SistemaVenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login lg = new Login();
+        lg.setVisible(true);
+    }
+    
+}
